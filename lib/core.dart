d0fb6efbe5131c1f402bfb7c8feebd096a362503
Hennn112 <mohamadaristian10@gmail.com>
2024-12-10 21:16:51 +0700

    First Initialization

---
+++ lib/core.dart
@@ -0,0 +1,23 @@
+// Core File - Mengelola Semua Ekspor
+
+// Binding
+export 'package:ids_project/module/picture/binding/picture_binding.dart';
+
+// Controllers
+export 'package:ids_project/module/picture/controller/detail_picture_controller.dart';
+export 'package:ids_project/module/picture/controller/list_picture_controller.dart';
+
+// Views
+export 'package:ids_project/module/picture/view/detail_picture_view.dart';
+export 'package:ids_project/module/picture/view/list_picture_view.dart';
+
+// Widgets
+export 'package:ids_project/module/picture/widget/detail_content_widget.dart';
+export 'package:ids_project/module/picture/widget/list_content_widget.dart';
+export 'package:ids_project/shared/widget/loading_widget.dart';
+
+// Services
+export 'package:ids_project/service/picture/picture_service.dart';
+
+// Routing
+export 'package:ids_project/routes/app_pages.dart';
